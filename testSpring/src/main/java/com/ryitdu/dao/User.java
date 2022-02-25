7c85ec6ef25d7475ec4976b3392fcd711b3637dd
ryitzhou <100122112+ryitzhou@users.noreply.github.com>
2022-02-25 17:36:19 +0800

    Update User.java
    添加了年龄和构造函数

--- testSpring/src/main/java/com/ryitdu/dao/User.java
+++ testSpring/src/main/java/com/ryitdu/dao/User.java
@@ -4,5 +4,12 @@ public class User {
     private String username;
 
     private String sex;
-
+    
+    private int age;
+    
+    User(String username,String sex,int age){
+        this.username=username;
+        this.sex=sex;
+        this.age=age;
+    }
 }
